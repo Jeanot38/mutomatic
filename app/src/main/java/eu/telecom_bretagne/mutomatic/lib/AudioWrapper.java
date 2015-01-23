9830c1f484e1aafd270f0161e780bb18cffcdf1b
Vincent L <vincent.longuepee@gmail.com>
2015-01-23 09:50:13 +0100

    Modification des méthodes de gestion du profil audio

--- app/src/main/java/eu/telecom_bretagne/mutomatic/lib/AudioWrapper.java
+++ app/src/main/java/eu/telecom_bretagne/mutomatic/lib/AudioWrapper.java
@@ -7,33 +7,46 @@ import android.media.AudioManager;
  * Created by Vincent on 22/01/2015.
  */
 public class AudioWrapper {
-    private Context context;
-    AudioManager audioSetting;
+    AudioManager audioWrapper;
+    int previousRingerMode;
 
-    public AudioWrapper(Context c){
-        this.context=c;
-        this.audioSetting=(AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    public AudioWrapper(Context c) {
+        Context context = c;
+        this.audioWrapper = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
     }
 
-    public int getCurrentSettings () {
-        int currentSettings =audioSetting.getRingerMode();
+    public int getCurrentSettings() {
+        int currentSettings = audioWrapper.getRingerMode();
         return currentSettings;
     }
 
-    public void setSettings (int i){
+    public void setSettings(int i) {
         String settings;
-        switch (i){
-            case 0: audioSetting.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-                settings="Silencieux";
+        switch (i) {
+            case 0:
+                audioWrapper.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                settings = "Silencieux";
                 break;
-            case 1: audioSetting.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-                settings="Vibreur";
+            case 1:
+                audioWrapper.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                settings = "Vibreur";
                 break;
-            case 2: audioSetting.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-                settings="Normal";
+            case 2:
+                audioWrapper.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                settings = "Normal";
                 break;
-            default:settings="Mode invalide";
+            default:
+                settings = "Mode invalide";
                 break;
         }
+    }
+
+    public void autoSettings() {
+        this.previousRingerMode=audioWrapper.getRingerMode();
+        audioWrapper.setRingerMode(AudioManager.RINGER_MODE_SILENT);   //Application du profil correspondant (silent par defaut)
+    }
+
+    public void restoreSettings(){
+        audioWrapper.setRingerMode(this.previousRingerMode);
     }
 }
