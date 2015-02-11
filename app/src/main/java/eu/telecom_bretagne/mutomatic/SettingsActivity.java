cd6cf125dca8a69294166024ea1ba3d7898db66c
Mathis Gavillon <mathis.gavillon@telecom-bretagne.eu>
2015-02-11 16:32:21 +0100

    - Adding option service interval refresh

--- app/src/main/java/eu/telecom_bretagne/mutomatic/SettingsActivity.java
+++ app/src/main/java/eu/telecom_bretagne/mutomatic/SettingsActivity.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Switch;
 
 import java.util.LinkedList;
@@ -71,13 +74,81 @@ public class SettingsActivity extends Activity {
             }
         });
 
-        LinkedList<Calendar> calendars = new LinkedList<>();
+        LinkedList<Calendar> calendars;
         CalendarWrapper calendarWrapper = new CalendarWrapper(getContentResolver());
         calendars= calendarWrapper.getCalendars();
         for (Calendar calendar : calendars){
             String calendarName = calendar.getName();
             CheckBox calendarCheckbox = new CheckBox(getApplicationContext());
             calendarCheckbox.setText(calendarName);
+        }
+
+        Spinner spinnerServiceInterval = (Spinner) findViewById(R.id.spinnerServiceInterval);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, SpinnerServiceIntervalMapping.getSpinnerDescriptions());
+
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerServiceInterval.setAdapter(spinnerAdapter);
+        spinnerServiceInterval.setSelection(SpinnerServiceIntervalMapping.getDefaultSpinnerPosition());
+
+        spinnerServiceInterval.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Integer parameterValue = SpinnerServiceIntervalMapping.getSpinnerValuesFromDescription((String) parent.getItemAtPosition(position));
+                if(parameterValue != null) {
+                    Parameters.setPreference(Parameters.SCHEDULING_INTERVAL, parameterValue);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private static class SpinnerServiceIntervalMapping {
+
+        private static String[] spinnerDescriptions = {"30 s", "1 min", "2 min", "5 min", "15 min", "30 min", "1 h"};
+        private static Integer[] spinnerValues = {30, 60, 120, 300, 900, 1800, 3600};
+
+        public static String[] getSpinnerDescriptions() {
+            return spinnerDescriptions;
+        }
+
+        public static Integer[] getSpinnerValues() {
+            return spinnerValues;
+        }
+
+        public static Integer getSpinnerValuesFromDescription(String spinnerValue) {
+            Integer indexOfSpinner = null;
+            for(int i=0 ; i < spinnerDescriptions.length ; i++) {
+                if(spinnerDescriptions[i].equals(spinnerValue)) {
+                    indexOfSpinner = i;
+                    break;
+                }
+            }
+
+            if(indexOfSpinner != null) {
+                return spinnerValues[indexOfSpinner];
+            }
+
+            return null;
+        }
+
+        public static int getDefaultSpinnerPosition() {
+
+            Integer serviceIntervalParameter = Parameters.getIntPreference(Parameters.SCHEDULING_INTERVAL);
+
+            if(serviceIntervalParameter != null) {
+                for (int i = 0; i < spinnerValues.length; i++) {
+                    if (spinnerValues[i].equals(serviceIntervalParameter)) {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
         }
     }
 
